ef261b0a7b70972478e25c42617061e77fe0b942
Anders Pitman <tapitman11@gmail.com>
2018-11-09 23:23:17 -0700

    Rendering texture

--- src/webgl_sim.js
+++ src/webgl_sim.js
@@ -2,9 +2,14 @@ const { timeNowSeconds } = require('./utils');
 
 const vsSource = `
   attribute vec2 aVertexPosition;
+  attribute vec2 aTexCoord;
+
+  varying vec2 vTexCoord;
 
   void main() {
     gl_Position = vec4(aVertexPosition, 0.0, 1.0);
+
+    vTexCoord = aTexCoord;
   }
 `;
 
@@ -12,18 +17,31 @@ const fsSource = `
 
   precision mediump float;
 
+  varying vec2 vTexCoord;
+
   uniform float uVal;
+  uniform sampler2D uTexture;
 
   void main() {
-    if (gl_FragCoord.x > uVal) {
-      gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
-    }
-    else {
-      gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);
-    }
+    //if (gl_FragCoord.x > uVal) {
+    //  gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
+    //}
+    //else {
+    //  gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);
+    //}
+
+    //gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);
+    gl_FragColor = texture2D(uTexture, vTexCoord);
   }
 `;
 
+// simple quad to cover entire canvas
+const QUAD = new Float32Array([
+  1, 1,
+  1, -1,
+  -1, 1,
+  -1, -1,
+]);
 
 
 export class WebGLSim {
@@ -37,6 +55,7 @@ export class WebGLSim {
     this.canvas.height = dim.height;
     this.$el.appendChild(this.canvas);
     this.gl = this.canvas.getContext('webgl');
+    const gl = this.gl;
 
     this.gl.clearColor(1.0, 1.0, 1.0, 1.0);
     this.gl.clear(this.gl.COLOR_BUFFER_BIT);
@@ -47,6 +66,7 @@ export class WebGLSim {
       program: shaderProgram,
       attribLocations: {
         vertexPosition: this.gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+        texCoordPosition: this.gl.getAttribLocation(shaderProgram, 'aTexCoord'),
       },
       uniformLocations: {
         uResolution: this.gl.getUniformLocation(shaderProgram, 'uResolution'),
@@ -55,48 +75,70 @@ export class WebGLSim {
       },
     };
 
-    this.initBuffers(this.gl);
-
-    // simple quad to cover entire canvas
-    const QUAD = new Float32Array([
-      1, 1,
-      1, -1,
-      -1, 1,
-      -1, -1,
-    ]);
-
-    this.gl.useProgram(this.defaultShaderInfo.program);
-
-    const gl = this.gl;
-    const numComponents = 2;
-    const type = gl.FLOAT;
-    const normalize = false;
-    const stride = 0;
-    const offset = 0;
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.positionBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, QUAD, gl.STATIC_DRAW);
-
-    gl.vertexAttribPointer(
-        this.defaultShaderInfo.attribLocations.vertexPosition,
-        numComponents,
-        type,
-        normalize,
-        stride,
-        offset);
-    gl.enableVertexAttribArray(
-        this.defaultShaderInfo.attribLocations.vertexPosition);
-
-    gl.drawArrays(gl.TRIANGLE_STRIP, offset, 4);
-
-    this._val = 0;
-  }
-
-  initBuffers(gl) {
+    //this.gl.useProgram(this.defaultShaderInfo.program);
 
     const positionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-    this.positionBuffer = positionBuffer;
+    gl.enableVertexAttribArray(
+        this.defaultShaderInfo.attribLocations.vertexPosition);
+    gl.vertexAttribPointer(
+        this.defaultShaderInfo.attribLocations.vertexPosition, 2, gl.FLOAT,
+        false, 0, 0);
+
+    gl.bufferData(gl.ARRAY_BUFFER, QUAD, gl.STATIC_DRAW);
+
+    //gl.drawArrays(gl.TRIANGLE_STRIP, offset, 4);
+
+    const texCoords = new Float32Array([
+      1, 0,
+      1, 1,
+      0, 0,
+      0, 1,
+    ]);
+
+    const texCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+
+    gl.enableVertexAttribArray(
+        this.defaultShaderInfo.attribLocations.texCoordPosition);
+    gl.vertexAttribPointer(
+        this.defaultShaderInfo.attribLocations.texCoordPosition, 2, gl.FLOAT,
+        false, 0, 0);
+    gl.bufferData(gl.ARRAY_BUFFER, texCoords, gl.STATIC_DRAW);
+
+    // set up texture
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+    const texData = new Uint8Array([
+      255, 0, 0, 255,
+      0, 255, 0, 255,
+      0, 0, 255, 255,
+      255, 0, 255, 255,
+    ]);
+
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 2, 2, 0, gl.RGBA,
+      //gl.UNSIGNED_BYTE, new Uint8Array([ 0, 0, 255, 255 ]));
+      gl.UNSIGNED_BYTE, texData);
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+    //gl.generateMipmap(gl.TEXTURE_2D);
+
+    //const image = new Image();
+    //image.src = '/dist/f-texture.png';
+    //image.addEventListener('load', function() {
+    //  gl.bindTexture(gl.TEXTURE_2D, texture);
+    //  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE,
+    //    image);
+    //  gl.generateMipmap(gl.TEXTURE_2D);
+    //});
+
+    this._val = 0;
   }
 
   step(grid, numRows, numCols) {
@@ -148,7 +190,7 @@ export class WebGLSim {
     //  //gl.drawArrays(gl.LINES, offset, grid.length / numComponents);
     //}
 
-    console.log("WebGL time: " + (timeNowSeconds() - startTime));
+    //console.log("WebGL time: " + (timeNowSeconds() - startTime));
   }
 }
 
